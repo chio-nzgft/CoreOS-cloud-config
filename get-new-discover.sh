06495169d65b4e0bc6edf0b9b2cb6e4022e9c2da
echochio <chio-nzgft@users.noreply.github.com>
2017-04-05 10:59:38 +0800

    Update get-new-discover.sh

--- get-new-discover.sh
+++ get-new-discover.sh
@@ -1,2 +1,3 @@
 export DISCOVERY="`curl -w "\n" 'https://discovery.etcd.io/new?size=1' | awk -F"/" '{ print $4 }'`"
 sed -i 's/b44a63fd67711fc1b19eaf6b18887664/'$(echo $DISCOVERY)'/g' cloud-config-node*.yaml
+echo $DISCOVERY
